6f1baf84133305e4f0e10aa3ca868fa3912cbc4d
Usha-U-G <ushagondakar@gmail.com>
2025-04-09 09:55:50 +0530

    save

---
+++ Substraction.java
@@ -0,0 +1,12 @@
+package java;
+class Substraction{
+public static void main(String[] args){
+    int a=10;
+    int b=5;
+    int sub= a-b;
+    System.out.println("the sum is:"+sub);
+
+}
+    
+}
+    
