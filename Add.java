402425b18f1ae1004de6369e3d612c5807136b57
Usha-U-G <ushagondakar@gmail.com>
2025-04-09 08:56:46 +0530

    save

--- Add.java
+++ Add.java
@@ -4,6 +4,8 @@ public static void main(String[] args){
     int a=10;
     int b=5;
     int sum= a+b;
+    int sub = a-b;
+    System.out.println("the sum is:"+sub);
     System.out.println("the sum is:"+sum);
 
 }
